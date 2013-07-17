64421053af3a964f345a43ab3220dc24f165e46d
the Soo-Rae <the.soorae@gmail.com>
2013-07-17 17:12:24 +0000

    corrected web.js

--- web.js
+++ web.js
@@ -2,12 +2,13 @@ var express = require('express');
 var fs=require('fs);
 
 var app = express.createServer(express.logger());
-var buf_contents = fs.readFileSync('index.html','utf-8');
-var str_contents = buf_contents.toString();
+
+
 
 
 app.get('/', function(request, response) {
-  response.send(str_contents);
+var html = fs.readFileSync("index.html).toString();
+response.send(html);
 });
 
 var port = process.env.PORT || 5000;
