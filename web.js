db7b0b4ebec488faea37a31110d9b3f2a368e8fd
the Soo-Rae <the.soorae@gmail.com>
2013-07-17 16:49:36 +0000

    changed web.js file

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readfile('/index.html', function(err, data){
+if (err) throw err;console.log(data);
 });
+app.get('/', function(request, response) {
+  response.send(data);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
