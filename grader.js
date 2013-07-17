576e3038957407f4c6d3ad2bee54ac96d4550d4d
the Soo-Rae <the.soorae@gmail.com>
2013-07-17 21:48:09 +0000

    update grader.js to take urls

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest=require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://thawing-ridge-9076.herokuapp.com/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +58,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(url, checksfile) {
+rest.get(url).on('complete',function(data) {
+	$ = cheerio.load(data);
+	var checks = loadChecks(checksfile).sort();
+	var out={};
+	for (var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	}
+	var outJson=JSON.stringify(out, null, 4);
+	console.log(outJson);
+});
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +82,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Path to url', clone(assertFileExists), URL_DEFAULT)
         .parse(process.argv);
+
+if (program.url) {
+	checkURL(program.url, program.checks);
+} else {checkHtmlFile (program.file, program.checks);
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-} else {
-    exports.checkHtmlFile = checkHtmlFile;
-}
+} 
