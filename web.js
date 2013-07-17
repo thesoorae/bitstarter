26f0fb234843c78e262eb273718a67dee9d4a4a8
the Soo-Rae <the.soorae@gmail.com>
2013-07-17 17:18:34 +0000

    web.js attempt 3

--- web.js
+++ web.js
@@ -4,10 +4,8 @@ var fs=require('fs);
 var app = express.createServer(express.logger());
 
 
-
-
 app.get('/', function(request, response) {
-var html = fs.readFileSync("index.html).toString();
+var html = fs.readFileSync("index.html", "utf-8");;
 response.send(html);
 });
 
